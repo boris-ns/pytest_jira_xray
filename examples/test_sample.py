bea9e201304fda64df9471e4f0253d8bb92165aa
Boris Sulicenko <b.sulicenko@gmail.com>
2020-08-06 20:52:45 +0200

    Added examples

---
+++ examples/test_sample.py
@@ -0,0 +1,42 @@
+import pytest
+
+def inc(x):
+    return x + 1
+
+
+def gt5(value):
+    return value > 5
+
+
+def test_true():
+    assert True
+
+def test_false():
+    assert True
+
+
+@pytest.mark.xray_test_id('DIP-2')
+def test_answer():
+    assert inc(3) == 4
+
+
+@pytest.mark.xray_test_id('DIP-3')
+def test_answer2():
+    assert inc(5) == 6
+
+
+@pytest.mark.xray_test_id('DIP-35')
+@pytest.mark.parametrize('value', [4, 5, 6, 7])
+def test_gt5(value):
+    assert gt5(value)
+
+
+"""
+Use this command to only run tests with this marker
+pytest -v -m my_mark
+ or 
+pytest -v -m "not my_mark" to run all tests except this one
+"""
+# @pytest.mark.my_mark
+# def test_with_marker():
+#     assert True
