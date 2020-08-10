25d0b1d0248c9668912e23d9ece5afcbd6b1c7f1
Boris Sulicenko <b.sulicenko@gmail.com>
2020-08-10 13:15:47 +0200

    Deleted unnecessary function

--- pytest_jira_xray/main.py
+++ pytest_jira_xray/main.py
@@ -16,50 +16,6 @@ XRAY_API_CLIENT_SECRET = os.environ.get('XRAY_API_CLIENT_SECRET')
 test_keys = {}
 
 
-def main():
-    token = get_authentication_token()
-
-    body = json.dumps({
-        "info": {
-            "summary": "Execution of automated tests for release v1.3",
-            "description": "This execution is automatically created when importing execution results from an external source",
-            "revision": "1.0.42134",
-            "startDate": "2014-08-30T11:47:35+01:00",
-            "finishDate": "2014-08-30T11:53:00+01:00",
-            "testPlanKey": "DIP-4",
-            "testEnvironments": []
-        },
-        "tests": [
-            {
-                "testKey": "DIP-2",
-                "start": "2014-08-30T11:47:35+01:00",
-                "finish": "2014-08-30T11:50:56+01:00",
-                "comment": "Successful execution",
-                "status": "PASSED"
-            },
-            {
-                "testKey": "DIP-3",
-                "start": "2014-08-30T11:47:35+01:00",
-                "finish": "2014-08-30T11:50:56+01:00",
-                "comment": "Successful execution",
-                "status": "FAILED"
-            }
-        ]
-    })
-
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
-    }
-
-    response = requests.post(XRAY_CREATE_TEST_EXECUTION_URL, data=body, headers=headers)
-
-    print(body)
-
-    print(response.status_code)
-    print(response.json())
-
-
 def get_authentication_token() -> str:
     headers = { 'Content-Type': 'application/json' }
 
