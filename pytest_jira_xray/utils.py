8fe24e52df27160fcdf475da9d6b3e5eff55e1af
Boris Sulicenko <b.sulicenko@gmail.com>
2020-08-25 23:37:14 +0200

    Fixed timezone

--- pytest_jira_xray/utils.py
+++ pytest_jira_xray/utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def get_current_datetime() -> str:
-    return datetime.now().strftime("%Y-%m-%dT%H:%M:%S+01:00")
+    return datetime.now().strftime("%Y-%m-%dT%H:%M:%S+02:00")
 
 def get_current_datetime_normal() -> str:
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
